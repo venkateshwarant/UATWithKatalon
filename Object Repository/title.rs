913311d5f3570cdd91e594cdda9b5d4a2dacfa62
Venkateshwaran <venkateshwarant@gmail.com>
2019-11-21 18:16:21 +0100

    New Test case

---
+++ Object Repository/title.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Hello world title box</description>
+   <name>title</name>
+   <tag></tag>
+   <elementGuidId>9c2aadc8-a1be-42df-9f8d-8bfcd38e772e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#hello</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Test Suites/HelloWorldTestSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HelloWorldTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6b1cb5fe-8489-40b9-b143-52c2d09bccc0</testSuiteGuid>
+   <testCaseLink>
+      <guid>0e41c55a-f3c2-4f9b-bc2a-4588625b3d42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Hello world test case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
